44d005e9ee94116e9dac8d08372dd12c56c6ab33
lufranski <lufranski@gmail.com>
2022-11-04 14:54:47 +0100

    Main ex + BONUS

---
+++ JS/main.js
@@ -0,0 +1,26 @@
+// Password Generator
+
+// Variabili
+const numeroPassword = 21;
+let nomeUtente;
+let cognomeUtente;
+let coloreUtente;
+
+// Chiedere all'utente il suo nome
+nomeUtente = prompt ("Inserisci il tuo nome");
+
+console.log(nomeUtente);
+
+// Chiedere all'utente il suo cognome
+cognomeUtente = prompt ("Inserisci il tuo cognome");
+
+console.log(cognomeUtente);
+
+// Chiedere all'utente il suo colore preferito
+coloreUtente = prompt ("Inserisci il tuo cognome preferito")
+
+console.log(coloreUtente);
+
+// Output
+
+document.getElementById("container").innerHTML = `Congratulazioni, hai creato la tua nuova password! La tua nuova password &eacute; ${nomeUtente}${cognomeUtente}${coloreUtente}${numeroPassword}! ` ;
